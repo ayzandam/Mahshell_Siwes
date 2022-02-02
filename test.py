6fccc9cff9200e66890c0b010e36790dc48a245e
ayzandam <ayzandam95@gmail.com>
2022-02-02 14:25:43 +0100

    Third Commit

--- test.py
+++ test.py
@@ -1,5 +1,45 @@
-num = 0
-for x in open("data.txt").readlines():
-    num += 1
+# num = 0
+# for x in open("data.txt").readlines():
+#     num += 1
 
-print(num)
+# print(num)
+
+class Student:
+    age = 0
+    allowance = 0
+
+makama = Student()
+aliyu = Student()
+ibrahim = Student()
+
+ages = [25, 30]
+allowance = [1000, 200, 400]
+
+class Student:
+    name
+    age
+
+class Staff:
+    name
+    age
+    this.name = name
+    self.name = name
+
+    def getname(self, par1, par2 ... parn)
+
+class Department:
+    name
+    faculty
+
+from test import Student => import Student from test
+from test import Student, Staff => import Student, Staff from test
+from test import * => import * from test
+
+
+import * from test
+
+object1.age = 20
+object1.sum(2, 9)
+
+def sum(x, y):
+    return (x+y)
